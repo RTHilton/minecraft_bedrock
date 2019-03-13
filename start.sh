82f91e7fb7111772ac576d64bd3e2839356ca1d9
RTHilton <ryan@pnwdc.com>
2019-03-13 09:17:28 -0700

    More logical checks to update container on image update

--- start.sh
+++ start.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-# Container would never update with this
-#if [ ! -f /data/bedrock_server ]; then
-#	echo "Extracting server..."
-#	unzip /opt/bedrock_server.zip -d /data
-#fi
-
-# We will always extract from the zip, it isn't that large
 cd /data
 
-# Backup configurations
-echo "Backing up configurations"
-mv server.properties server.properties.bak
-mv whitelist.json whitelist.json.bak
-mv permissions.json permissions.json.bak
+# Check if we have never initialized this container
+if [ ! -f /data/bedrock_server ]; then
+	echo "Extracting server..."
+	unzip /opt/bedrock_server.zip -d /data
+else
+  md5local=$(md5sum bedrock_server | cut -d' ' -f 1)
+  md5zip=$(unzip -p /opt/bedrock_server.zip  bedrock_server | md5sum | cut -d' ' -f 1)
 
-echo "Extracting server..."
-unzip /opt/bedrock_server.zip -o -d /data
+  # Check if the zip file in the image is different than our local volume
+  if [ $md5local != $md5zip ]; then
+    # Server was updated
+    # Backup configurations
+    echo "Backing up configurations"
+    mv server.properties server.properties.bak
+    mv whitelist.json whitelist.json.bak
+    mv permissions.json permissions.json.bak
 
-echo "Restoring configurations"
-mv server.properties.bak server.properties
-mv whitelist.json.bak whitelist.json
-mv permissions.json.bak permissions.json
+    echo "Extracting server..."
+    unzip /opt/bedrock_server.zip -o -d /data
+
+    echo "Restoring configurations"
+    mv server.properties.bak server.properties
+    mv whitelist.json.bak whitelist.json
+    mv permissions.json.bak permissions.json
+  fi
+fi
 
 echo "Starting Server..."
 LD_LIBRARY_PATH=. ./bedrock_server
